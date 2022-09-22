5e36890cf06e9e672fe993896f86f64a3f1a39de
ThaiTuanBTT <tuanbtgch200717@fpt.edu.vn>
2022-09-22 16:29:13 +0700

    ok

---
+++ superman.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supermans</title>
+</head>
+<body>
+    <picture>
+        <source srcset="sourceset" type="image/svg+xml">
+        <img src="https://i.ytimg.com/vi/C-pyMcda5Lw/maxresdefault.jpg" alt="image desc">
+      </picture>
+</body>
+</html>
